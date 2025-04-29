d72bf19d69adde5eb6c7f7cbe678536c519ab8d4
MD: Miel Mahmud Sifat <mahmudsifat2793@gmail.com>
2025-04-29 12:01:12 +0600

    update pb 4

---
+++ pb_4.cpp
@@ -0,0 +1,16 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    
+     long long  A,B,C,D;
+      cin>>A>>B>>C>>D;
+     long long  x= (A*B)-(C*D);
+     
+      cout<<"Difference = "<<x<<endl;
+         
+      
+
+
+}

eb0187e1127ca39955f8ca672cf264b517d7a769
MD: Miel Mahmud Sifat <mahmudsifat2793@gmail.com>
2025-04-29 12:14:17 +0600

    update  with problem 5

---
+++ pb5.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<iomanip>
+using  namespace std;
+int main()
+{
+
+     double  R;
+     cin>>R;
+      double area =(R*R*3.141592653);
+       cout<<fixed<<setprecision(9)<<area<<endl;
+}
--- pb_4.cpp
+++ pb_4.cpp
@@ -1,16 +1,15 @@
-
 #include<iostream>
 using namespace std;
 int main()
 {
-    
+
      long long  A,B,C,D;
       cin>>A>>B>>C>>D;
      long long  x= (A*B)-(C*D);
-     
+
       cout<<"Difference = "<<x<<endl;
-         
-      
+
+
 
 
 }
